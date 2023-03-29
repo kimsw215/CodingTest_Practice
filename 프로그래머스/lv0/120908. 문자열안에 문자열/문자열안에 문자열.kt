68fb8983367387107e62c995887229f9d38fd268
Sdoubleu <74901089+kimsw215@users.noreply.github.com>
2023-03-29 21:28:02 +0900

    Update 문자열안에 문자열.kt

--- 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.kt
+++ 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.kt
@@ -1,5 +1,5 @@
 class Solution {
     fun solution(str1: String, str2: String): Int {
-        return if(str1.contains(str2)) return 1 else return 2
+        return if(str1.contains(str2)) 1 else 2
     }
 }
