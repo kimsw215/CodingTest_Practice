147f78dbaee845809537c4556e3e48120f6a757f
Sdoubleu <74901089+kimsw215@users.noreply.github.com>
2023-03-30 23:33:08 +0900

    [level 0] Title: 대문자와 소문자, Time: 15.18 ms, Memory: 61 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.kt
@@ -0,0 +1,7 @@
+class Solution {
+    fun solution(my_string: String): String {
+        var answer: String = ""
+        for( i in my_string) if(i.toInt() <= 90) answer += i.toLowerCase() else answer += i.toUpperCase()
+        return answer
+    }
+}
