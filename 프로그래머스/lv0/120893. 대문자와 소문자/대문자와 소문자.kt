3eb5195209202622c7f3981d8d8b520dd13e6468
Sdoubleu <74901089+kimsw215@users.noreply.github.com>
2023-03-30 23:33:29 +0900

    Update 대문자와 소문자.kt

--- 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.kt
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.kt
@@ -2,6 +2,7 @@ class Solution {
     fun solution(my_string: String): String {
         var answer: String = ""
         for( i in my_string) if(i.toInt() <= 90) answer += i.toLowerCase() else answer += i.toUpperCase()
+        // if(i.isUpperCase()) c += i.toLowerCase()  else c += i.toUpperCase()
         return answer
     }
 }
