3e2dd91bbdbf6ff9ccc0fde3c0f87eadefa57b6a
Sdoubleu <74901089+kimsw215@users.noreply.github.com>
2023-04-04 12:11:15 +0900

    Update 최댓값 만들기 （2）.kt

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.kt
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.kt
@@ -4,5 +4,7 @@ class Solution {
         val b = numbers.sorted().takeLast(2).fold(1){acc, i -> acc * i}
     
         if(a >= b) return a else return b
+        // if 문을 사용하지 않고 max문으로도 사용 가능하다
+        // return Math.max(numbers.sorted().take(2).fold(1){acc, i -> acc * i}, numbers.sorted().takeLast(2).fold(1){acc, i -> acc * i})
     }
 }
