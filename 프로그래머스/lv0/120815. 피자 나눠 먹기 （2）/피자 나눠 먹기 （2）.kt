c6d79076c4010e0706d1e1e266354bf3777c95c7
Sdoubleu <74901089+kimsw215@users.noreply.github.com>
2023-04-05 13:05:01 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 11.85 ms, Memory: 61 MB -BaekjoonHub

--- 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.kt
+++ 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.kt
@@ -1,14 +1,13 @@
 class Solution {
     fun solution(n: Int): Int {
-        var answer = 0
-        for(i in 1..n){
-            if( (6*i) % n == 0 ){
-            answer = i
-            break
-            }
-        }
-        return answer
-        // return (1..n).filter{ 6*it % n == 0 }.first()
-        // 한 줄로 할 수도 있음 , 다만 시간이 70배 정도 늘어났음
+        // var answer = 0
+        // for(i in 1..n){
+        //     if( (6*i) % n == 0 ){
+        //     answer = i
+        //     break
+        //     }
+        // }
+        // return answer
+        return (1..n).asSequence().filter { 6*it % n == 0 }.first()
     }
 }
