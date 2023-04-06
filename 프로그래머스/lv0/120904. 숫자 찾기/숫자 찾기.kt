b250048bbbf5f910b6fe0d9fab6fcdb6570266a7
Sdoubleu <74901089+kimsw215@users.noreply.github.com>
2023-04-06 20:48:58 +0900

    [level 0] Title: 숫자 찾기, Time: 19.11 ms, Memory: 60.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.kt
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.kt
@@ -1,7 +1,5 @@
 class Solution {
     fun solution(num: Int, k: Int): Int {
-        if(num.toString().indexOf(k.toString()) != -1) 
-            return num.toString().indexOf(k.toString()) + 1
-        else return -1
+    return ("-"+num).toString().indexOf(k.toString())    
     }
 }
