ceb5ce4ee3f052b4db4258c8fb98bb0013fafeca
Sdoubleu <74901089+kimsw215@users.noreply.github.com>
2023-04-19 15:16:28 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 0.03 ms, Memory: 62.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.kt
+++ 프로그래머스/lv0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.kt
@@ -1,7 +1,5 @@
 class Solution {
     fun solution(dots: Array<IntArray>): Int {
-        var newDots = dots
-        newDots.sortBy { it[0] }
-        return Math.abs(newDots[1][1]-newDots[0][1]) * Math.abs(newDots[3][0] - newDots[1][0])
+        return Math.abs(dots[1][1]-dots[0][1]) * Math.abs(dots[2][0] - dots[1][0])
     }
 }
