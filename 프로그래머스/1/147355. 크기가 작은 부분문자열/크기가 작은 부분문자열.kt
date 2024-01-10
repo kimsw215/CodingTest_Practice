1d2d5c78d573bab5dc80506a5e81ed28d88f36c7
Sdoubleu <74901089+kimsw215@users.noreply.github.com>
2024-01-10 15:44:03 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 16.88 ms, Memory: 60.2 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.kt
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.kt
@@ -1,14 +1,11 @@
 class Solution {
     fun solution(t: String, p: String): Int {
-        
-        val mut = t.map { i -> i.toString().toLong() }
+
         val com = mutableListOf<Long>()
         
         for(i in 0 .. t.length - p.length){
             var num = ""
-            for(j in 0 until  p.length){
-                num += t[i+j].toString()
-            }
+            for(j in 0 until  p.length) num += t[i+j].toString()
             com.add(num.toLong())
         }
         
