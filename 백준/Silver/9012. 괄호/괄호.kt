e987f75290e953b72a56caf758b95fea6fc9f8cb
Sdoubleu <74901089+kimsw215@users.noreply.github.com>
2024-01-13 15:56:35 +0900

    [Silver IV] Title: 괄호, Time: 100 ms, Memory: 12468 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.kt
@@ -0,0 +1,23 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.lang.Exception
+import java.util.Stack
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val n = readLine().toInt()
+
+    repeat(n) {
+        val ps = readLine()
+        val stack: Stack<Char> = Stack()
+
+        try {
+            ps.forEach {
+                if (it == '(') stack.push('C')
+                else stack.pop()
+            }
+            if (stack.isEmpty()) println("YES")
+            else println("NO")
+        }
+        catch (e: Exception) { println("NO") }
+    }
+}
