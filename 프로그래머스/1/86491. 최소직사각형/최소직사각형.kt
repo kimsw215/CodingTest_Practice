8ed6695d166af886a4ea0b577f8c340f85f742eb
Sdoubleu <74901089+kimsw215@users.noreply.github.com>
2024-03-01 21:42:08 +0900

    [level 1] Title: 최소직사각형, Time: 2.17 ms, Memory: 68.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.kt
@@ -0,0 +1,24 @@
+class Solution {
+    fun solution(sizes: Array<IntArray>): Int {
+        var hMax = sizes[0][0]
+        var wMin = sizes[0][0]
+        for (i in sizes) {
+            val max = if (i[0] <= i[1]) i[1] else i[0]
+            val min = if (i[0] <= i[1]) i[0] else i[1]
+            if (hMax <= max) hMax = max
+            if (wMin >= min) wMin = min
+        }
+        
+        var maxOfMin = 0
+        for (i in sizes) {
+            if(i[0] < i[1]) { 
+                if( maxOfMin <= i[0]) maxOfMin = i[0] 
+            } 
+            else { 
+                if( maxOfMin <= i[1]) maxOfMin = i[1] 
+            }
+    }
+        
+        return hMax * maxOfMin
+    }
+}
