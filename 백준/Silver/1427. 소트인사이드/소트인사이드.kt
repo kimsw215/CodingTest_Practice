e581d16943ea1b22b8d05d825d6e0a0916ee35fa
Sdoubleu <74901089+kimsw215@users.noreply.github.com>
2025-05-28 18:44:54 +0900

    [Silver V] Title: 소트인사이드, Time: 152 ms, Memory: 21440 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.kt
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.kt
@@ -1,14 +1,6 @@
-import java.io.BufferedReader
-import java.io.InputStreamReader
-
 fun main() {
-    val br = BufferedReader(InputStreamReader(System.`in`))
-    val bw = System.out.bufferedWriter()
-    val Nk = readln().toMutableList()
-    Nk.sort()
-    Nk.reverse()
-    bw.write("${Nk.joinToString("")}")
-    bw.flush()
-    bw.close()
-    br.close()
+    val n = readln().toMutableList()
+    n.sort()
+    n.reverse()
+    println(n.joinToString(""))
 }
