f52736434ac77ad3c7959f193d4da43f41518dee
Sdoubleu <74901089+kimsw215@users.noreply.github.com>
2025-06-11 18:24:22 +0900

    [Silver IV] Title: 문자열 집합, Time: 640 ms, Memory: 88024 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.kt
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.kt
@@ -1,15 +1,12 @@
 fun main() {
-    val NM = readln().split(" ").map{ it.toInt() }
-    val n = NM[0]
-    val m = NM[1]
-    val nArr = mutableListOf<String>()
-    val mArr = mutableListOf<String>()
-    var cnt = 0
-    for(i in 0 until n) nArr.add(readln())
-    for(i in 0 until m) mArr.add(readln())
-    val mArrMap = mArr.withIndex().groupBy({it.value}, {it.index}).mapValues{ it.value.size }
-    for(i in mArrMap) {
-        if(i.key in nArr) cnt+= i.value
+    val (n, m) = readln().split(" ").map{ it.toInt() }
+    val nArr = HashSet<String>()
+    repeat(n) {
+        nArr.add(readln())
     }
-    println(cnt)
+    var cnt = 0
+    repeat(m) {
+        if(nArr.contains(readln())) cnt++
+    }
+    print(cnt)
 }
