333ccc87a77b2bd594fcbe681e0c5e369e852e2f
Sdoubleu <74901089+kimsw215@users.noreply.github.com>
2026-08-24 16:06:48 +0900

    [level 1] Title: 햄버거 만들기, Time: 37.18 ms, Memory: 110 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.kt
@@ -0,0 +1,23 @@
+class Solution {
+    fun solution(ingredient: IntArray): Int {
+        var answer: Int = 0
+        val stack = ArrayDeque<Int>()
+        for(i in ingredient) {
+            stack.addLast(i)
+            if(stack.size >= 4 &&
+                    stack[stack.size - 4] == 1 &&
+                    stack[stack.size - 3] == 2 &&
+                    stack[stack.size - 2] == 3 &&
+                    stack[stack.size - 1] == 1
+                    ) {
+                stack.removeLast()
+                stack.removeLast()
+                stack.removeLast()
+                stack.removeLast()
+                answer++
+            }
+        }
+    
+        return answer
+    }
+}
